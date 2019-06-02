08e002c3837235f726c246392c412f185b61ed82
gbaman <gbaman@users.noreply.github.com>
2019-06-02 20:27:35 +0100

    Add sudo to install file copy

--- ballymacrickett/install.sh
+++ ballymacrickett/install.sh
@@ -5,4 +5,4 @@ sudo apt install python3-pytrack
 
 sudo pip3 install -r requirements.txt
 
-cp tracker.service /lib/systemd/system/tracker.service -f
+sudo cp tracker.service /lib/systemd/system/tracker.service -f
